967bfad80828eed711e4d77aa0164680e89b54ef
Kenaniah Cerny <kenaniah@gmail.com>
2016-02-07 12:13:25 -0800

    adds paginated? helper method

--- lib/querify/paginate.rb
+++ lib/querify/paginate.rb
@@ -1,6 +1,17 @@
 module Querify
 	module Paginate
 
+		def self.included mod
+			mod.instance_eval do
+				attr_accessor :_querify_paginated
+				_querify_paginated = false
+			end
+		end
+
+		def paginated?
+			_querify_paginated ? true : false
+		end
+
 		# Accepts the following options:
 		# - :per_page The number of results returned per page (bound by max and min)
 		# - :min_per_page The minimum number of results per page
@@ -8,11 +19,8 @@ module Querify
 		def paginate options = {}
 
 			# Determine config options
-			options[:per_page] = options.fetch(:per_page).to_i rescue Querify.config.per_page
-			options[:per_page] = 20 if options[:per_page].to_i < 1
-
-			options[:min_per_page] = options.fetch(:min_per_page).to_i rescue Querify.config.min_per_page
-			options[:min_per_page] = 20 if options[:min_per_page].to_i < 1
+			options[:per_page] = options.fetch(:per_page).to_i rescue Querify.config.per_page || 20
+			options[:min_per_page] = options.fetch(:min_per_page).to_i rescue Querify.config.min_per_page || 20
 
 			options[:max_per_page] = nil if options[:max_per_page] == 0
 			unless options.has_key?(:max_per_page) && options[:max_per_page].nil?
@@ -22,31 +30,46 @@ module Querify
 
 			# Determine the current page
 			current_page = 1
-			current_page = $params[:page].to_i if $params[:page].to_i > 0
+			if defined? $params[:page]
+				current_page = $params[:page].to_i if $params[:page].to_i > 0 rescue current_page
+			end
+			if options.has_key? :page
+				current_page = options[:page].to_i if options[:page].to_i > 0 rescue current_page
+			end
 
 			# Determine results per page
-			per_page = $params[:per_page].to_i if $params[:per_page].to_i != 0
+			per_page = 0
+			if options.has_key? :per_page
+				per_page = options[:per_page].to_i rescue per_page
+			end
+			if defined? $params[:per_page]
+				per_page = $params[:per_page].to_i rescue per_page
+			end
 
 			# Skip if there is no need to paginate
-			return self if max_per_page.nil? && per_page < 0
+			return self if options[:max_per_page].nil? && per_page < 1
 
 			# Account for the max & min per page
 			per_page = [per_page, options[:max_per_page]].min unless options[:max_per_page].nil?
 			per_page = [per_page, options[:min_per_page]].max
-			$response.headers["X-Per-Page"] = per_page
-			$response.headers["X-Current-Page"] = current_page
 
-			# Also set pagination statistic headers when requested
-			if $params[:page_stats] == "1"
-				total = self.size
-				$response.headers['X-Total-Pages'] = (total.to_f / per_page).ceil
-				$response.headers['X-Total-Results'] = total
+			if defined? $response
+				$response.headers["X-Per-Page"] = per_page
+				$response.headers["X-Current-Page"] = current_page
+
+				# Also set pagination statistic headers when requested
+				if $params[:page_stats] == "1"
+					total = self.size
+					$response.headers['X-Total-Pages'] = (total.to_f / per_page).ceil
+					$response.headers['X-Total-Results'] = total
+				end
 			end
 
 			# Perform limits
 			limited = self.limit(per_page).offset(per_page * (current_page - 1))
 
 			# Return the limited query
+			limited._querify_paginated = true
 			return limited
 
 		end
