5945f008cdbe8209435fcf5e7576c7e3bb7bcfe3
Kenaniah Cerny <kenaniah@gmail.com>
2016-02-13 14:56:12 -0800

    Reduced the amount of factory girl instances

--- test/paginate_test.rb
+++ test/paginate_test.rb
@@ -3,17 +3,22 @@ require 'test_helper'
 describe Querify::Paginate do
 
 	before(:all) do
-		100.times do
-			FactoryGirl.create(:post)
-		end
+		FactoryGirl.create(:post)
 	end
 
-	it 'ActiveRecord responds to paginate' do
+	it 'ActiveRecord responds to #paginate' do
 		assert_respond_to Post, :paginate
-		assert_respond_to Post.all.first.comments, :paginate
+		assert_respond_to Post.all, :paginate
+		assert_respond_to Post.first.comments, :paginate
 	end
 
-	it 'ActiveRecord knows the query is paginated' do
+	it 'ActiveRecord responds to #paginated?' do
+		assert_respond_to Post, :paginated?
+		assert_respond_to Post.all, :paginated?
+		assert_respond_to Post.first.comments, :paginated?
+	end
+
+	it 'ActiveRecord knows if the query is paginated' do
 		assert Post.paginate.paginated?
 		assert_equal false, Post.paginated?
 	end
