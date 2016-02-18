1fa1f0604018786c7d34422c48c7f00934a66502
David Perel <dmperel@gmail.com>
2016-02-18 10:16:19 -0500

    Delete integration tests. Rename querify_test to filterable_test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,42 +21,8 @@ Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-class ActiveSupport::TestCase
-    class << self
-      alias :context :describe
-    end
-end
-
 def truncate_db
 	Author.destroy_all
 	Post.destroy_all
 	Comment.destroy_all
 end
-
-# module TestHelper
-#
-#     def configure_querify
-#         Querify.config.per_page = 20
-#         Querify.config.min_per_page = 10
-#         Querify.config.max_per_page = 50
-#     end
-#
-#     def clear_params
-#         Querify.params.clear
-#     end
-#
-#     def jsonify
-#         return json = JSON.parse(response.body)
-#     end
-#
-#     def setup_data
-#         100.times do
-#             FactoryGirl.create(:post)
-#         end
-#
-#         30.times do
-#             FactoryGirl.create(:comment)
-#         end
-#     end
-#
-# end
